19c6ad5631dcd63c2fd11dc9182bc0dde38d736b
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-09-17 17:11:41 +0530

    changes made to  app config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         //
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
     }
 
     /**
@@ -24,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
+
 }
